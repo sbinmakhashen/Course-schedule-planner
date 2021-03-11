e829cd8956067df7641c2f15264960e38b32ad88
Salem <nlsabm@yahoo.com>
2021-03-11 00:04:11 -0500

    UI and js constructors

---
+++ ES5-app.js
@@ -0,0 +1,19 @@
+// Project is created using ES5 in this file
+
+// course constructor
+function Course(subject, instructor, term) {
+  this.subject = subject;
+  this.instructor = instructor;
+  this.term = term;
+}
+
+// creating UI constructor
+function UI() {
+
+}
+
+
+// Listen for submit
+document.getElementById('course-form').addEventListener('submit', function () {
+
+});
